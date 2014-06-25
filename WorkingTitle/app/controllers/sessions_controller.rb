f856a1862d5158865ddee4ee0dd45dbe7d944041
Ahmed Al-Bahar & Daniel Yoachim <pair+aalba005.dyoachim@devbootcamp.com>
2014-06-25 17:31:33 -0500

    user specs complete

--- WorkingTitle/app/controllers/sessions_controller.rb
+++ WorkingTitle/app/controllers/sessions_controller.rb
@@ -2,6 +2,10 @@ class SessionsController < ApplicationController
 
   def login
     @user = User.find_by_email(params[:email])
+    Rails.logger.info("-----------")
+    Rails.logger.debug("-----------")
+    puts '--------------'
+
     if @user && @user.authenticate(params[:password])
       session[:current_user_id] = @user.id
       redirect_to user_path(@user)
