527b619d353862d48bf2be8bfe78a9e38a2a9c80
NatalieFrecka <nfrecka@gmail.com>
2014-06-25 11:32:25 -0500

    Added seed file, nav bar, and search method in books model

--- WorkingTitle/app/models/book.rb
+++ WorkingTitle/app/models/book.rb
@@ -2,4 +2,14 @@ class Book < ActiveRecord::Base
 	has_many :votes
 	has_many :comments
 	belongs_to :user
+
+	private
+
+	def self.search(search)
+	  if search
+	    find(:all, conditions: ['title LIKE ?', "%#{search}%"])
+	  else
+	    find(:all)
+	  end
+	end
 end
