c3b9752590da24f626cab93002e1f3a6e1a8d6df
Ahmed Al-Bahar & Daniel Yoachim <pair+aalba005.dyoachim@devbootcamp.com>
2014-06-27 14:39:48 -0500

    add user page content, add index content

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,15 @@
 class WelcomeController < ApplicationController
+
+  def index
+    @books = Book.all
+    @recent_books = Book.order(created_at: :desc).limit(5)
+    @recent_comments = Comment.order(created_at: :desc).limit(5)
+    @best_level = Book.get_highest_level_book
+    @most_words = Book.get_most_words_book
+    @best_word_length = Book.get_highest_word_length_book
+    @best_syllable_length = Book.get_most_syllable_book
+    @best_sentence_length = Book.get_longest_sentences_book
+    @most_author = Book.group(:author).count.first
+    @most_book = Book.group(:title).count.first
+  end
 end
--- spec/books/book_spec.rb
+++ spec/books/book_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Book do
 
 	let(:book){Book.create(title: "test-book")}
-	
+
 	describe '#get_word_count' do
 		it 'returns the word count' do
 			expect(book.get_word_count).to eq(11)
@@ -46,6 +46,42 @@ describe Book do
 		end
 	end
 
+	describe 'leading book methods' do
+		let!(:sherlock_book){Book.create(title: "sherlock")}
+		let!(:kafka_book){Book.create(title: "kafka")}
+		let!(:ulysses_book){Book.create(title: "ulysses")}
+
+		describe '#get_highest_level_book' do
+			it "returns the book with the highest reading level" do
+				expect(Book.get_highest_level_book).to eq(kafka_book)
+			end
+		end
+
+		describe '#get_most_words_book' do
+			it "returns the book with the highest word count" do
+				expect(Book.get_most_words_book).to eq(ulysses_book)
+			end
+		end
+
+		describe '#get_highest_word_length_book' do
+			it "returns the book with the longest average word length" do
+				expect(Book.get_highest_word_length_book).to eq(ulysses_book)
+			end
+		end
+
+		describe '#get_most_syllable_book' do
+			it "returns the book with the longest average syllables per word" do
+				expect(Book.get_most_syllable_book).to eq(ulysses_book)
+			end
+		end
+
+		describe '#get_longest_sentences_book' do
+			it "returns the book with the longest average sentences" do
+				expect(Book.get_longest_sentences_book).to eq(kafka_book)
+			end
+		end
+	end
+
 	describe '#parse_into_csv' do
 		it 'parses raw text into a csv == |name,count|' do
 			book.parse_into_csv
@@ -53,17 +89,17 @@ describe Book do
 			File.open("#{Rails.root}/public/p-test-book.txt") do |t|
 				text = t.read
 			end
-			
+
 			expect(text[11..14]).to eq("is,2")
 		end
 
-		it 'should have headers' do 
+		it 'should have headers' do
 			book.parse_into_csv
 			text = ""
 			File.open("#{Rails.root}/public/p-test-book.txt") do |t|
 				text = t.read
 			end
-			
+
 			expect(text[0..9]).to eq("name,count")
 		end
 	end
