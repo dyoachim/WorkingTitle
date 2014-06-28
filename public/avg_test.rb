6958be44f3fdf58e70b60ea824aaa5ed19889628
aalba005 <aalba005@fiu.edu>
2014-06-28 16:13:14 -0500

    d3 working. First draft. Code needs cleaning

--- public/avg_test.rb
+++ public/avg_test.rb
@@ -1,4 +1,5 @@
 require "csv"
+require "json"
 
 class String
   def syllable_count
@@ -62,7 +63,8 @@ def avg_syllable_length
 end
 
 def parse_into_csv
-  File.open("test-book.txt") do |t|
+    stop_words = ["the", "a", "about", "above", "above", "across", "-", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount", "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as", "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+  File.open("kafka.txt") do |t|
     # array of words
     words = t.read.downcase.gsub(/[,.\/!@#$%^&*()_1234567890\[\]|'":;<>?`~+=]/, "").split(" ")
     
@@ -73,10 +75,21 @@ def parse_into_csv
     chunked_words = chunked_words.sort {|x,y| y[1] <=> x[1]}
 
     # put chunks into csv file
-    CSV.open("test.csv", "w", write_headers: true, headers: ["name", "count"]) do |csv|
+    json_array = []
+    json_chunks = []
+    CSV.open("kafka.csv", "w", write_headers: true, headers: ["name", "count"]) do |csv|
       chunked_words.each do |chunk|
         csv << chunk
-      end
+        json_chunks  = {name: chunk[0], size: chunk[1]} 
+          # puts json_chunks[:name]
+        json_array << json_chunks unless stop_words.include? json_chunks[:name]      
+          end
+        json_array = json_array.first(30)
+        File.open("flare.json","w") do |f|
+        f.write('{ "children": ')
+        f.write(json_array.to_json)
+        f.write("}")
+      end 
     end
   end
 end
@@ -90,3 +103,4 @@ def get_reading_level
 end
 
 puts get_reading_level
+parse_into_csv
