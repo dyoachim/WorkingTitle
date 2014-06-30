0b16144c0dbb2e92c3fb0c09a5555297dd62ccc2
Daniel Yoachim <dyoachim@gmail.com>
2014-06-29 21:20:17 -0500

    minor navigation fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require_tree .
 //= require d3
 
-$(document).on('page:load',function() {
+$(document).on("page:load",function() {
 	var url = window.location.href;
 
 	$('.downvote').on('click', function(e) {
